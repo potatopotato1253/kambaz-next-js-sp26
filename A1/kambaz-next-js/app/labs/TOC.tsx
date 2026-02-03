d139a0562a069530225f8090e9641c9d7313152a
potato <huynh.vie@northeastern.edu>
2026-02-03 15:29:06 -0500

    update 2.4.17

--- A1/kambaz-next-js/app/labs/TOC.tsx
+++ A1/kambaz-next-js/app/labs/TOC.tsx
@@ -1,19 +1,36 @@
 "use client";
 
 import { Nav, NavItem, NavLink } from "react-bootstrap";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function TOC() {
+  const pathname = usePathname();
+
   return (
     <Nav variant="pills" className="justify-content-center mb-3">
-      <NavItem><NavLink href="/labs">Labs</NavLink></NavItem>
-      <NavItem><NavLink href="/labs/lab1">Lab 1</NavLink></NavItem>
-      <NavItem><NavLink href="/labs/lab2">Lab 2</NavLink></NavItem>
-      <NavItem><NavLink href="/labs/lab3">Lab 3</NavLink></NavItem>
-      <NavItem><NavLink href="/">Kambaz</NavLink></NavItem>
       <NavItem>
-        <NavLink href="https://github.com/potatopotato1253/kambaz-next-js-sp26">
-          My GitHub
-        </NavLink>
+        <NavLink as={Link} href="/labs" active={pathname === "/labs"}>Labs</NavLink>
+      </NavItem>
+
+      <NavItem>
+        <NavLink as={Link} href="/labs/lab1" active={pathname === "/labs/lab1"}>Lab 1</NavLink>
+      </NavItem>
+
+      <NavItem>
+        <NavLink as={Link} href="/labs/lab2" active={pathname === "/labs/lab2"}>Lab 2</NavLink>
+      </NavItem>
+
+      <NavItem>
+        <NavLink as={Link} href="/labs/lab3" active={pathname === "/labs/lab3"}>Lab 3</NavLink>
+      </NavItem>
+
+      <NavItem>
+        <NavLink as={Link} href="/">Kambaz</NavLink>
+      </NavItem>
+
+      <NavItem>
+        <NavLink href="https://github.com/potatopotato1253/kambaz-next-js-sp26">My GitHub</NavLink>
       </NavItem>
     </Nav>
   );
